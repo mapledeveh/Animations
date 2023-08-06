5a6067aef9dc3290c30636f62c9e44473f738ebd
mapledeveh <mapledeveh@gmail.com>
2023-08-06 18:41:53 -0400

    Update Progress

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Alex Nguyen on 2023-05-13.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
